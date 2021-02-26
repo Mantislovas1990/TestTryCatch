ccc7beccac85d261cff5709c4006094154f18397
Mantas <mantislovas24@gmail.com>
2021-02-26 15:50:20 +0200

    added functions / VaziuoklesIsskleidimoKlaida

--- src/testTryCatch/VaziuoklesIsskleidimoKlaida.java
+++ src/testTryCatch/VaziuoklesIsskleidimoKlaida.java
@@ -3,7 +3,11 @@ package testTryCatch;
 public class VaziuoklesIsskleidimoKlaida extends Exception {
     private Exception priezastis;
 
+    public VaziuoklesIsskleidimoKlaida(Exception priezastis) {
+        this.priezastis = priezastis;
+    }
 
-
-
+    public Exception getPriezastis() {
+        return this.priezastis;
+    }
 }
